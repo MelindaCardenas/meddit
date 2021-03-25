e2a82f261eca0e1c1098f131ad26eb7198253de3
Melinda Cardenas <mc6377@nyu.edu>
2021-03-25 14:21:14 -0400

    add skeleton

---
+++ migrations/20210325155726_posts.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('posts', function (table) {
+        table.increments('id')
+        table.string('text')
+        table.string('user_id')
+        table.timestamp('created_at')
+      })
+};
+
+exports.down = function(knex) {
+  
+};
