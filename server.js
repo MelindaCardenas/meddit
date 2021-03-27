0f7c45c3e33f65faf7aa11a7f2efdf8d00e4f968
seolacarrano <seola6@gmail.com>
2021-03-27 16:31:46 -0400

    finished all CRUD

--- server.js
+++ server.js
@@ -10,20 +10,18 @@ const PORT = process.env.PORT || 3000
 //************* users ****************/
 // get all users 
 app.get('/users', async (req, res) => {
-    try {
-      const users = await db.any(`select * from users`)
-      console.log(users)
-      return res.json(users)
-    } catch (err) {
-      res.status(500).send(err)
-    }
+  try {
+    //.any, .none etc are pgpromise method / pgpromise is in db.js
+    const users = await db.any(`select * from users`)
+    return res.json(users)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 })
 
 // create a user
 app.post('/users', async (req, res) => {
   try {
-    //db.none: execute this query, but no need to return anything 
-    //db.none already knows that we want to take out ${full_name} from req.body 
     await db.none('insert into users (username) values (${username})', req.body)
 
     return res.json({
@@ -36,12 +34,21 @@ app.post('/users', async (req, res) => {
 })
 
 //************* posts ****************
-
 //get all posts
 app.get('/posts', async (req, res) => {
   try {
-    //.any, .none etc are pgpromise method / pgpromise is in db.js
-    const posts = await db.any(`select * from posts`)
+    const posts = await db.any('select * from posts')
+    return res.json(posts)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+//get one post
+app.get('/posts/:postId', async (req, res) => {
+  const postId = parseInt(req.params.postId, 10);
+  try {
+    const posts = await db.any('select * from posts where id=$1', postId)
     return res.json(posts)
   } catch (err) {
     res.status(500).send(err)
@@ -49,15 +56,9 @@ app.get('/posts', async (req, res) => {
 })
 
 //create a post
-app.post('/posts/:userId', async (req, res) => {
-  const userId = parseInt(req.params.userId, 10);
+app.post('/posts', async (req, res) => {
   try {
-    //db.none: execute this query, but no need to return anything 
-    //db.none already knows that we want to take out ${full_name} from req.body 
-    if (userId) {
-      await db.none('insert into posts (text) values (${text})', req.body)
-    }
-
+    await db.none('insert into posts (user_id, text) values (${user_id}, ${text})', req.body)
 
     return res.json({
       message: 'success'
@@ -69,11 +70,10 @@ app.post('/posts/:userId', async (req, res) => {
 })
 
 //update a post
-app.patch('/posts', async (req, res) => {
+app.patch('/posts/:postId', async (req, res) => {
   const postId = parseInt(req.params.postId, 10);
 
   try {
-    //$1 = req.body.name, $2=req.body.email, $3 = req.body.eamil
     await db.none('update posts set text=$1 where id = $2', [req.body.text, postId])
 
     res.json({
@@ -91,8 +91,50 @@ app.delete('/posts/:postId', async (req, res) => {
   const postId = parseInt(req.params.postId, 10);
 
   try {
-    //$1 = studentId in line 67
     await db.none('delete from posts where id = $1', postId)
+    res.status(204).json({
+      message: 'success'
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+//************* comments ****************/
+//get comments
+app.get('/posts/:postId/comments', async (req, res) => {
+  const postId = req.params.postId;
+
+  try {
+    const comments = await db.any('select * from comments where post_id=$1', postId)
+    return res.json(comments)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+//create a comment
+app.post('/posts/:postId/comments', async (req, res) => {
+  try {
+    await db.none('INSERT INTO comments (user_id, post_id, text) VALUES (${user_id}, ${post_id}, ${text})', req.body)
+
+    return res.json({
+      message: 'success'
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+//delete a commment
+app.delete('/posts/:postId/comments/:commentId', async (req, res) => {
+  const commentId = parseInt(req.params.commentId, 10);
+
+  try {
+    //$1 = studentId in line 67
+    await db.none('delete from comments where id = $1', commentId)
     res.status(204).json({
       message: 'success'
     })
@@ -105,3 +147,4 @@ app.delete('/posts/:postId', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`listenining on http://localhost:${PORT}`)
 })
+
